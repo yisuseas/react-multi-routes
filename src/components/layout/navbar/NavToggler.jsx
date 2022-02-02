04e9f3c2f38d3075d37549dcc5525105066faa13
yisuseas <yisuseas@gmail.com>
2022-02-02 08:00:04 -0600

    Added Burger Menu

--- src/components/layout/navbar/NavToggler.jsx
+++ src/components/layout/navbar/NavToggler.jsx
@@ -1,13 +1,62 @@
 import React from "react";
+import { useSpring, animated, config } from "react-spring";
+
+function BurgerCross(props) {
+  const { isCross } = props;
+  const { stage } = useSpring({
+    stage: isCross ? 0 : 100,
+    config: config.gentle,
+  });
+
+  return (
+    <svg
+      width="30"
+      height="30"
+      viewBox="0 0 7.9374998 7.9375002"
+      version="1.1"
+      id="svg8"
+    >
+      <animated.path
+        className="menu-svg-path"
+        d={stage.to({
+          range: [0, 50, 100],
+          output: [
+            "M 3.96875,3.96875 H 3.96875",
+            "M 2.3812502,3.96875 H 5.55625",
+            "M 0.52916667,3.96875 H 7.14375",
+          ],
+        })}
+      />
+      <animated.path
+        className="menu-svg-path"
+        d={stage.to({
+          range: [0, 50, 100],
+          output: [
+            "m 0.7937494,0.79375 c 0,0 1.3229173,1.3229167 3.1750006,3.175 1.8520833,-1.8520833 3.175,-3.175 3.175,-3.175",
+            "m 0.7937494,0.79375 c 0,0 1.0583333,1.5875 3.1750001,1.5875 2.1166667,0 3.1750005,-1.5875 3.1750005,-1.5875",
+            "m 0.7937494,0.79375 c 0,0 0.2645839,0 3.1750006,0 2.9104167,0 3.175,0 3.175,0",
+          ],
+        })}
+      />
+      <animated.path
+        className="menu-svg-path"
+        d={stage.to({
+          range: [0, 50, 100],
+          output: [
+            "m 0.7937494,7.14375 c 0,0 1.3229173,-1.3229167 3.1750006,-3.175 1.8520833,1.8520833 3.175,3.175 3.175,3.175",
+            "m 0.79375009,7.14375 c 0,0 1.05833331,-1.5875 3.17500001,-1.5875 2.1166666,0 3.1749999,1.5875 3.1749999,1.5875",
+            "m 0.79375009,7.14375 c 0,0 0.26458321,0 3.17499991,0 2.9104167,0 3.175,0 3.175,0",
+          ],
+        })}
+      />
+    </svg>
+  );
+}
 
 function NavToggler(props) {
   const { open, onClick } = props;
 
-  const icon = open ? (
-    <i className="fas fa-times"></i>
-  ) : (
-    <i className="fas fa-bars"></i>
-  );
+  const icon = <BurgerCross isCross={open} />;
 
   return (
     <div className="flex md:hidden order-first col-span-1 items-center">
--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,35 +1,73 @@
 import React, { useState } from "react";
+import { useSpring, animated, config } from "react-spring";
+
+function BurgerCross(props) {
+  const { stage } = useSpring({
+    stage: props.on ? 100 : 0,
+    config: config.gentle,
+  });
+
+  return (
+    <svg
+      width="30"
+      height="30"
+      viewBox="0 0 7.9374998 7.9375002"
+      version="1.1"
+      id="svg8"
+    >
+      <animated.path
+        className="menu-svg-path"
+        d={stage.to({
+          range: [0, 50, 100],
+          output: [
+            "M 3.96875,3.96875 H 3.96875",
+            "M 2.3812502,3.96875 H 5.55625",
+            "M 0.52916667,3.96875 H 7.14375",
+          ],
+        })}
+      />
+      <animated.path
+        className="menu-svg-path"
+        d={stage.to({
+          range: [0, 50, 100],
+          output: [
+            "m 0.7937494,0.79375 c 0,0 1.3229173,1.3229167 3.1750006,3.175 1.8520833,-1.8520833 3.175,-3.175 3.175,-3.175",
+            "m 0.7937494,0.79375 c 0,0 1.0583333,1.5875 3.1750001,1.5875 2.1166667,0 3.1750005,-1.5875 3.1750005,-1.5875",
+            "m 0.7937494,0.79375 c 0,0 0.2645839,0 3.1750006,0 2.9104167,0 3.175,0 3.175,0",
+          ],
+        })}
+      />
+      <animated.path
+        className="menu-svg-path"
+        d={stage.to({
+          range: [0, 50, 100],
+          output: [
+            "m 0.7937494,7.14375 c 0,0 1.3229173,-1.3229167 3.1750006,-3.175 1.8520833,1.8520833 3.175,3.175 3.175,3.175",
+            "m 0.79375009,7.14375 c 0,0 1.05833331,-1.5875 3.17500001,-1.5875 2.1166666,0 3.1749999,1.5875 3.1749999,1.5875",
+            "m 0.79375009,7.14375 c 0,0 0.26458321,0 3.17499991,0 2.9104167,0 3.175,0 3.175,0",
+          ],
+        })}
+      />
+    </svg>
+  );
+}
 
 function About() {
-  const [open, setOpen] = useState(false);
+  const [on, setOn] = useState(true);
 
-  const toggleDiv = () => {
-    setOpen(!open);
+  const toggleOn = () => {
+    setOn(!on);
   };
 
   return (
-    <div className="">
+    <div className="mx-10">
       <h1>This is About</h1>
-      <div className="bg-black text-white flex flex-col p-2">
-        <button onClick={() => toggleDiv()}>
-          <i className="fas fa-times"></i>
+      <div>
+        <button className="bg-red-500 w-6 h-6" onClick={() => toggleOn()}>
+          X
         </button>
-        <div className="overflow-hidden">
-          <div className="transition-all mt-[-100px] open:mt-0" open={open}>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci
-              quas a blanditiis, molestiae culpa tempora. Vero totam aspernatur
-              amet rem impedit explicabo itaque repellat reiciendis quod sequi
-              ad, laborum porro.
-            </p>
-            <p>
-              Sequi placeat odit cupiditate ullam molestias voluptatibus ad
-              voluptas? Sint, consectetur quibusdam, sed veritatis quam id
-              expedita quo rem sunt molestiae, laboriosam praesentium.
-              Distinctio fugiat, repellat corrupti perferendis doloribus
-              laboriosam!
-            </p>
-          </div>
+        <div className="bg-black">
+          <BurgerCross on={on} />
         </div>
       </div>
     </div>

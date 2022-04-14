cee2015ba206463e669ba7a35732547985da652f
yisuseas <yisuseas@gmail.com>
2022-04-14 16:56:42 -0500

    smol

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <footer className="mt-auto text-center bg-slate-900 text-gray-500">
-      ©Mon-Massage, 2022
+      ©Mon-Massage, 2022 | We have changed this
     </footer>
   );
 }
